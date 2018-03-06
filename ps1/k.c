aceec1b92e3f9f8a946feb06182c84e21b6df3b9
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-03-06 17:38:42 +0100

    kcko

--- ps1/k.c
+++ ps1/k.c
@@ -17,3 +17,37 @@ void add_random_tile(struct game *game){
         game->board[row][col] = 'B';
     }
 }
+
+bool is_move_possible(const struct game game){
+	for (int i=0;i<SIZE; i++){
+	       for (int j=0; j <SIZE; j++){
+		       if (j<(SIZE-1) && i <(SIZE-1)){
+			       if(game.board[i][j] == game.board[i][j+1] || game.board[i][j] == game.board[i+1][j]){
+				       return true;
+			       }
+		       }
+		       else if(j ==(SIZE-1) && i < (SIZE-1)){
+			       if(game.board[i][j] == game.board[i+1][j]){
+				       return true;
+			       }
+		       }
+		       else if(i == (SIZE-1) && j < (SIZE-1)){
+			       if(game.board[i][j] == game.board [i][j+1]){
+				       return true;
+			       }
+		       }
+	       }
+	}
+	return false;
+}
+
+bool is_game_won(const struct game game){
+	for (int i=0; i < SIZE; i++){
+		for (int j=0; j < SIZE; j++){
+			if(game.board[i][j]== 'K'){
+				return true;
+			}
+		}
+	}
+	return false;
+}

cd83df74a28b3dac732504d27fd08fd9e318adb1
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-03-24 11:43:23 +0100

    dasda

--- ps2/bmp.c
+++ ps2/bmp.c
@@ -80,7 +80,7 @@ if(text == NULL || key == NULL) return NULL;
 				encrypt[i] = 64 + (toupper(encrypt[i]) + helpp - 'Z');
 			}
 			else encrypt[i] = toupper(encrypt[i]) + helpp;
-			j++;
+			 j++;
 		}
 	}
 	free(help);

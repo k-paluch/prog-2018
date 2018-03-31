f53a8b199123d6a54832de346b6bf19a47373e69
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-03-31 17:16:05 +0200

    dsdaa

--- ps3/mm.c
+++ ps3/mm.c
@@ -16,19 +16,19 @@ int main(){
 	int i;
 	scanf("%ld",&number);
 	while((count(number))>1){
-	dig= count(number);
-	char arr[dig];
-	int c=0;
-	while(dig--){
-		arr[dig]=number%10;
-		number/=10;
-		c++;
-	}
-	arr[c]='\0';
-	number=0;
-	for(i=0;i<c;i++){
-		number+=arr[i];
-	}
+		dig= count(number);
+		char arr[dig];
+		int c=0;
+		while(dig--){
+			arr[dig]=number%10;
+			number/=10;
+			c++;
+		}
+		arr[c]='\0';
+		number=0;
+		for(i=0;i<c;i++){
+			number+=arr[i];
+		}
 	}
 	printf("%ld\n",number);
 	return 0;

f42d477abc19e6e20fe9ac21acec3cbd41c027a5
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-05-13 12:12:27 +0200

    Upload New File

---
+++ ps4/command.c
@@ -0,0 +1,27 @@
+#include"command.h"
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+struct command* create_command(char* name, char* description, char* pattern, size_t nmatch)
+{
+	if(name == NULL || description == NULL){
+		return NULL;
+	}
+	int nlen = strlen(name);
+	int dlen = strlen(description);
+	if(nlen == 0 || dlen == 0){
+		return NULL;
+	}
+	struct command* command = (struct command*)calloc(1, sizeof(struct command));
+	
+	command->name = name;
+	command->description = description;
+	
+	return command;
+}
+
+struct command* destroy_command(struct command* command)
+{
+	free(command);
+	return NULL;
+}

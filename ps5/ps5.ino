1c2a76ab403f1dca27d70374e5f4577c9c987b8c
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-05-13 22:31:10 +0200

    Upload New File

---
+++ ps5/ps5.ino
@@ -0,0 +1,47 @@
+#include "mastermind.h"
+#include"lcd_wrapper.h"
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+ pinMode(BTN_1_PIN, INPUT);
+ pinMode(BTN_2_PIN, INPUT);
+ pinMode(BTN_3_PIN, INPUT);
+ pinMode(BTN_4_PIN, INPUT);
+ pinMode(BTN_ENTER_PIN, INPUT);
+ 
+ pinMode(LED_BLUE_1, OUTPUT);
+ pinMode(LED_RED_1, OUTPUT);
+ pinMode(LED_BLUE_2, OUTPUT);
+ pinMode(LED_RED_2, OUTPUT);
+ pinMode(LED_BLUE_3, OUTPUT);
+ pinMode(LED_RED_3, OUTPUT);
+ pinMode(LED_BLUE_4, OUTPUT);
+ pinMode(LED_RED_4, OUTPUT);
+ char n[] = {"Mastermind"};
+  lcd.init();
+  lcd.backlight();
+  for (int i = 0; i < strlen(n); i++) {
+    lcd.setCursor(i, 0);
+    lcd.print(n[i]);
+  }  
+  lcd.init();                                                                             
+  lcd.backlight();
+  Serial.begin(9600);
+  Serial.println(' ');
+}
+
+void loop() {
+  lcd.backlight();
+  lcd.clear();
+  char* code = generate_code(false, 4);
+  for (int i = 0; i < 4; i++) {
+    lcd.print((int)code[i]);
+    delay(1000);
+  }
+  delay(5555);
+  play_game(code);
+  free(code);
+
+}

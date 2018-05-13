db51bf91cff733de6cf39ea9628d9b71bf33cc5e
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-05-13 12:27:32 +0200

    aaaaaaaaaaaaaaleluja

--- ps4/game.c
+++ ps4/game.c
@@ -1,1 +1,96 @@
-#include"game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game.h"
+#include "parser.h"
+#include "world.h"
+
+void play_game(struct game* game)
+{
+	
+}
+
+struct game* create_game()
+{
+	struct game* game = (struct game*)calloc(1, sizeof(struct game));
+	game->parser = create_parser();
+	game->world = create_world();
+	game->current_room = NULL;
+	game->backpack = NULL;
+	return game;
+}
+
+struct game* destroy_game(struct game* game)
+{
+	destroy_parser(game->parser);
+	destroy_world(game->world);
+	free(game);
+	return NULL;
+}
+
+void execute_command(struct game* game, struct command* command)
+{
+	if(game == NULL){
+		return;
+	}
+	game->state = PLAYING;
+	
+	if(command == NULL){
+		return;
+	}
+	struct game *new = game;
+	
+	if(strcmp(command->name, "SEVER") == 0)
+	{
+		if(new->current_room->north != NULL)
+			new->current_room = new->current_room->north;
+	}
+	else if(strcmp(command->name, "JUH") == 0)
+	{
+		if(new->current_room->south != NULL)
+			new->current_room = new->current_room->south;
+	}
+	else if(strcmp(command->name, "ZAPAD") == 0)
+	{
+		if(new->current_room->west != NULL)
+			new->current_room = new->current_room->west;
+	}
+	else if(strcmp(command->name, "VYCHOD") == 0)
+	{
+		if(new->current_room->east != NULL)
+			new->current_room = new->current_room->east;
+	}
+	else if(strcmp(command->name, "ROZHLIADNI SA") == 0)
+	{
+		show_room(new->current_room);
+	}
+	else if(strcmp(command->name, "PRIKAZY") == 0)
+	{
+		printf("Zoznam prikazov:\n");
+		struct container* next_con = NULL;
+		for(struct container *cursor = game->parser->commands; cursor != NULL; cursor = next_con)
+		{
+			next_con = cursor->next;
+			printf("%s\t%s\n", cursor->command->name, cursor->command->description);
+		}
+	}
+	else if(strcmp(command->name, "VERZIA") == 0)
+	{
+		printf("Verzia 1.0.3\n");
+	}
+	else if(strcmp(command->name, "RESTART") == 0)
+	{
+		new->state = RESTART;
+		destroy_game(new);
+		create_game();
+	}
+	else if(strcmp(command->name, "O HRE") == 0)
+	{
+		printf("Kracas si ulicou a zrazu bolest a tma.\n");
+	}
+	else if(strcmp(command->name, "KONIEC") == 0)
+	{
+		new->state = GAMEOVER;
+		destroy_game(new);
+	}
+}
--- ps4/main.c
+++ ps4/main.c
@@ -1,5 +1,71 @@
 #include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "container.h"
+#include "room.h"
+#include "parser.h"
+#include "world.h"
+#include "game.h"
 
-int main(){
+int main()
+{
+	struct room* start = create_room("start", "Nic nevidis, no na zemi je baterka.");
+	struct room* library = create_room("library", "Vsade len same knihy, no ziadne dvere.");
+		
+	set_exits_from_room(start, library, NULL, NULL, NULL);
+	set_exits_from_room(library, NULL, start, NULL, NULL);
+	
+	show_room(start);
+	show_room(library);
+	
+	
+	printf("start %s _|||_ %s\n", start->name, start->description);
+		
+	struct container* first = NULL;
+	
+	first = create_container(first, ROOM, start);
+	printf("first %s _|||_ %s\n", first->room->name, first->room->description);
+	first = create_container(first, ROOM, library);
+	printf("first %s _|||_ %s\n", first->next->room->name, first->next->room->description);
+	
+	set_exits_from_room(first->room, first->next->room, NULL, NULL, NULL);
+	set_exits_from_room(first->next->room, NULL, first->room, NULL, NULL);
+	
+	destroy_room(start);
+	destroy_room(library);
+	destroy_containers(first, ROOM);
+	
+	
+	struct parser *balik = create_parser();
+	
+	printf("north %s\n", balik->commands->next->next->command->description);
+	
+	char* input = " SEVER                         ";
+	struct command* cmd = parse_input(balik, input);
+	if(cmd != NULL)
+	{
+		printf("HAAAALELUJA\n");
+	}
+	
+	balik = destroy_parser(balik);
+	
+	struct game *gamee = create_game();
+	gamee = destroy_game(gamee);
+	struct container* world = create_world();
+	world = destroy_world(world);
+	
+	struct game* game = create_game();
+	struct room* severna = create_room("north room", "Tato miestnost je na severe.");
+	struct room* juzna = create_room("south room", "Tato miestnost je na juhu.");
+	set_exits_from_room(severna, NULL, juzna, NULL, NULL);
+	set_exits_from_room(juzna, severna, NULL, NULL, NULL);
+	game->current_room = severna;
+	printf("current room %s\n", game->current_room->name);
+	struct command* command = parse_input(game->parser, "  Commands  ");
+	execute_command(game, command);
+	printf("current room after %s\n", game->current_room->name);
+	destroy_game(game);
+	free(severna);
+	free(juzna);
 	return 0;
 }
